70ccbcef0b74527c03be917b77081df412802f62
owen <owen2781379@gmail.com>
2026-03-14 00:35:11 -0400

    [UPDATE] teardown script to include redis

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 CASSANDRA="$SCRIPT_DIR/../cassandra"
+REDIS="$SCRIPT_DIR/../redis"
 SPRING="$SCRIPT_DIR/../URLShortener"
 
 echo "Tearing down Spring Boot..."
 kubectl delete -f "$SPRING/spring-deployment.yaml" --ignore-not-found
 kubectl delete -f "$SPRING/spring-service.yaml" --ignore-not-found
+
+echo "Tearing down Redis..."
+kubectl delete -f "$REDIS/redis-replica-deployment.yaml" --ignore-not-found
+kubectl delete -f "$REDIS/redis-primary-deployment.yaml" --ignore-not-found
+kubectl delete -f "$REDIS/redis-service.yaml" --ignore-not-found
 
 echo "Tearing down Cassandra..."
 kubectl delete -f "$CASSANDRA/cassandra-statefulset.yaml" --ignore-not-found
